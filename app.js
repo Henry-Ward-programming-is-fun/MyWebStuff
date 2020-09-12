b9f3974c431add56f1ac5ec22858661b8b675c57
Henry Ward <67829371+Henry-Ward-programming-is-fun@users.noreply.github.com>
2020-09-12 16:04:39 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+var greeting = document.getElementById('greeting');
+var explanation = document.getElementById('explaining');
+/*
+* TODOs
+*/
+document.getElementById('sayHiButton').addEventListener('click', function(){alert('hi!')}); greeting.innerHTML = 'Hello. This is Henry. And This is my website.'; explanation.innerHTML = '            This is my website. I hope you like it.  :)\n Also made with Less (Not just yet... but soon!), HTML, CSS, and JavaScript'
